9942becf015509cf2603e990dcbb35c63b86c154
Jacob M Sock <jacobmsock@gmail.com>
2023-03-24 13:56:24 -0400

    Initial Commit

---
+++ NarcissisticNumber/NarcissisticNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  NarcissisticNumberApp.swift
+//  NarcissisticNumber
+//
+//  Created by Jacob Sock on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct NarcissisticNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
